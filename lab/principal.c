a7d9ff5c0adf91664c83ec85a4757486d9885c93
AdrianUah <adrian.valcarcel@edu.uah.es>
2023-11-07 16:17:54 +0100

    version a

---
+++ lab/principal.c
@@ -0,0 +1,26 @@
+version
+
+# Por favor ingresa el mensaje del commit para tus cambios. Las
+#  líneas que comiencen con '#' serán ignoradas, y un mensaje
+#  vacío aborta el commit.
+#
+# En la rama master
+#
+# Confirmación inicial
+#
+# Cambios a ser confirmados:
+#	nuevos archivos: principal.c
+#
+# Archivos sin seguimiento:
+#	../README.md
+#	../funciones.h
+#	../laboratorio/
+#	../principal.c
+#	../principal.o
+#	../prueba.c
+#	../shared
+#	../so/
+#	../sumar.c
+#	../sumar.h
+#	../sumar.o
+#
